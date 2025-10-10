9e598d6320f7a88ca309e72c05025f98e5ef1819
Jertrude Fides Seguisa <fides.seguisa@signatureip.ai>
2025-10-09 21:38:20 -0700

    Set clock freq to 2GHz
    Signed-off-by: Jertrude Fides Seguisa <fides.seguisa@signatureip.ai>

--- amba_chi/src/ClkResetIfx.cpp
+++ amba_chi/src/ClkResetIfx.cpp
@@ -2,5 +2,5 @@
 
 ClkResetIfx::ClkResetIfx(sc_core::sc_module_name name)
     : sc_core::sc_module(name)
-    , main_clock_out("master_clk", 10, sc_core::SC_NS)
+    , main_clock_out("master_clk", 500, sc_core::SC_PS)
 {}
